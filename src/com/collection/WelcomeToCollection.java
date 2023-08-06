a89c97e889d60a3ee775b81a75c49dd2c40d770e
Phool1995 <pbs3611@gmail.com>
2023-08-06 11:35:16 +0530

    first commit

---
+++ src/com/collection/WelcomeToCollection.java
@@ -0,0 +1,12 @@
+package com.collection;
+
+public class WelcomeToCollection {
+
+	public static void main(String[] args) {
+		System.out.println("hi collection");
+		System.out.println("By by collection");
+		// TODO Auto-generated method stub
+
+	}
+
+}
